a61e1f3ab522ca423a847759497abeed1b2e759b
RadioMan116 <noris75564@gmail.com>
2020-09-16 16:21:53 +0300

    callback через props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,12 +5,17 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
 
 	let postsElement = props.posts.map(p => <Post message={p.message} likeCount={p.likeCount} avatar={p.avatar} />)
+	let newPostElement = React.createRef();
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		console.log(text)
+	}
 	return (
 		<div className={s.postsBlocks}>
 			<h3> My posts</h3>
 			<div>
-				<textarea></textarea>
-				<div><button>Add post</button></div>
+				<textarea ref={newPostElement}></textarea>
+				<div><button onClick={addPost}>Add post</button></div>
 
 			</div>
 			<div className={s.posts}>
