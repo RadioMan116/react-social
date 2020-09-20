e60f4fb0639d5ddc1de373cc795900b0068691b3
RadioMan116 <noris75564@gmail.com>
2020-09-20 11:18:30 +0300

    dispatch и action

---
+++ build/precache-manifest.3fe9a1acb700d2142ebeb204450cc6f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e563d7a81fa918367f1e",
+    "url": "/static/css/main.19e71922.chunk.css"
+  },
+  {
+    "revision": "e563d7a81fa918367f1e",
+    "url": "/static/js/main.e563d7a8.chunk.js"
+  },
+  {
+    "revision": "c71138d15dced3a9356d",
+    "url": "/static/js/1.c71138d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a3de070498e4c639c12e369b07543946",
+    "url": "/index.html"
+  }
+];
